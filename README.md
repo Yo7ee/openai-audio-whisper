e8c283b3558e1388bd4525715b254cc4e7e09629
YC Chen <yo7chen@gmail.com>
2023-11-07 21:52:38 +0800

    update Readme

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# openai-audio-whisper
+# OpenAI-audio-whisper
+
+## Overview
+
+The OpenAI-audio-whisper project provides two functions: `slice_audio` and `audio_transcription`. These functions offer convenient audio processing capabilities.
+
+### `slice_audio`
+
+`slice_audio` allows you to effortlessly divide an audio file into smaller, manageable slices. This function is useful for tasks such as transcription or analyzing specific segments of audio.
+
+**Required parameters:**
+
+- `filename`: The name of the audio file.
+- `time`: The duration of each slice.
+- `directory`: The location where the audio slices will be saved.
+
+### `audio_transcription`
+
+`audio_transcription` converts audio files into text, facilitating tasks like generating transcripts from spoken content.
+
+**Required parameter:**
+
+- `file_path`: The path to the audio file for transcription.
+
+## Project Customization
+
+Within this project, you have the flexibility to customize the "slice_data" or "data" folder according to your specific needs.
+
+## Supported Audio Format
+
+Please note that the `audio_transcription` function currently only supports the mp3 format.
+
+---
+
+_If you have any questions or encounter any issues, please don't hesitate to reach out to us._
--- main.py
+++ main.py
@@ -4,53 +4,38 @@ import openai
 from dotenv import load_dotenv
 from pydub import AudioSegment
 
-import argparse
-
-def create_parser():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '-s', '--slice_audio',
-        type = str,
-        help = 'To slice the audio to fit the volumne under 25MB.'
-    )
-
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# folder path setting
-# put original audio
-data_dir = os.path.join(os.path.dirname(__file__), 'data')
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+SLICE_DATA_DIR = os.path.join(os.path.dirname(__file__), 'slice_data')
+TRANSCRIPT_AND_AUDIO_FILENAME= "2327_183_20231026"
 
-# put slice audio
-slice_data_dir = os.path.join(os.path.dirname(__file__), 'slice_data')
-
-def audio_transcription(filepath):
+def audio_transcription(filepath, exportFilename):
     audio_file= open(filepath, "rb")
     transcript = openai.Audio.transcribe("whisper-1", audio_file) #{"test":"audio content"}
 
-    #transcript = { "text": "abc adf asd {0}".format(filepath)} #測試用
     write_into_txt = "{0} ".format(transcript["text"])
-    with open("2327_183_20231026.txt", 'a+') as f:
+    with open("{0}.txt".format(exportFilename), 'a+') as f:
         f.write(write_into_txt)
 
-def slice_audio(filename, minutes, slice_data_dir):
-    data_path = os.path.join(data_dir, "{0}.mp3".format(filename))
+def slice_audio(filename, minutes, SLICE_DATA_DIR):
+    data_path = os.path.join(DATA_DIR, "{0}.mp3".format(filename))
     song = AudioSegment.from_mp3(data_path)
 
     # PyDub handles time in milliseconds
     slice_minutes = minutes * 60 * 1000
 
     for i, chunk in enumerate(song[::slice_minutes]):
-        filename_with_path = os.path.join(slice_data_dir, filename)
+        filename_with_path = os.path.join(SLICE_DATA_DIR, filename)
         chunk.export("{0}_{1}.mp3".format(filename_with_path, i), format="mp3")
 
 
 if __name__ == "__main__":
-    slice_audio("2327_183_20231026", 15, slice_data_dir)
+    slice_audio(TRANSCRIPT_AND_AUDIO_FILENAME, 15, SLICE_DATA_DIR)
 
-    # slice_files = os.listdir(slice_data_dir) #[xxx, xxx, xxx]
-    # for filename in slice_files:
-    #     filepath = os.path.join(slice_data_dir, filename)
-    #     audio_transcription(filepath)
+    slice_files = os.listdir(SLICE_DATA_DIR) #[xxx, xxx, xxx]
+    for filename in slice_files:
+        filepath = os.path.join(SLICE_DATA_DIR, filename)
+        audio_transcription(filepath, TRANSCRIPT_AND_AUDIO_FILENAME)
